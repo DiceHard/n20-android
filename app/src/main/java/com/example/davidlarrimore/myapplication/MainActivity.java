a9e7b042f4b68de20dd1a1542cd0c92f98280b65
IanDCarroll <ian.d.carroll@gmail.com>
2016-07-21 11:53:41 -0700

    make button round and change styling

--- app/src/main/java/com/example/davidlarrimore/myapplication/MainActivity.java
+++ app/src/main/java/com/example/davidlarrimore/myapplication/MainActivity.java
@@ -28,15 +28,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         TextView textView=(TextView) findViewById(R.id.textView2);
         textView.setText(String.valueOf(die.getRoll()));
     }
